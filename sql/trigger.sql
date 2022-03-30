74eff6fad989edc160beeb6875c32efad50e7e60
ErSongHeng <song.heng@yahoo.com.sg>
2022-03-30 22:44:17 +0800

    Update trigger.sql

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,9 +1,14 @@
 CREATE TRIGGER rating_system
 AFTER UPDATE ON ratings
 FOR EACH ROW
-EXECUTE PROCEDURE change_ratings()
+EXECUTE PROCEDURE change_ratings();
 
 CREATE TRIGGER initialiser
 AFTER INSERT ON users
 FOR EACH ROW
-EXECUTE PROCEDURE initialise()
+EXECUTE PROCEDURE initialise();
+
+CREATE TRIGGER transaction_to_rating
+AFTER UPDATE ON transactions
+FOR EACH ROW
+EXECUTE PROCEDURE rate_transaction();
